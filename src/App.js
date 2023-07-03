f04f8eb176336950365d92876fa24a2e24e1a07a
Mariya Kashif <mariyakashif@Mariyas-MacBook-Air.local>
2023-07-03 13:11:35 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setFilteredUsers(data);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }, []);
+
+  const handleSearch = (event) => {
+    const searchText = event.target.value;
+    setSearchText(searchText);
+
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setFilteredUsers(filteredUsers);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    
+    <div>
+        <input
+          className="search"
+          type="text"
+          placeholder="Search your contacts..."
+          value={searchText}
+          onChange={handleSearch}
+        />
+        
+      </div>
+      <table> 
+        
+      <thead>
+      <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Address</th> 
+      </tr>
+      </thead>
+      <tbody>
+      
+        {filteredUsers.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                {user.address.city}, {user.address.country}
+              </td>
+            </tr>
+        ))}
+        
+      </tbody>
+      </table>
     </div>
   );
 }
